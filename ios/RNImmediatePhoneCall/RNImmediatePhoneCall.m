53649efd8f00a9960e189e802aec8807837d25f6
Ishaan Sharma <admin@scizers.com>
2024-10-07 21:16:48 +0400

    Update RNImmediatePhoneCall.m
    added code for ios18

--- ios/RNImmediatePhoneCall/RNImmediatePhoneCall.m
+++ ios/RNImmediatePhoneCall/RNImmediatePhoneCall.m
@@ -1,8 +1,9 @@
 #import <UIKit/UIKit.h>
+
 #import "RCTBridgeModule.h"
 
-
 @interface RNImmediatePhoneCall : NSObject <RCTBridgeModule>
+
 @end
 
 @implementation RNImmediatePhoneCall
@@ -10,10 +11,35 @@
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(immediatePhoneCall:(NSString *)number)
+
 {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"tel:%@", number]]];
-    });
-};
+
+    NSString *phoneUrlString = [NSString stringWithFormat:@"tel:%@", number];
+
+    NSURL *phoneUrl = [NSURL URLWithString:phoneUrlString];
+
+    if ([[UIApplication sharedApplication] canOpenURL:phoneUrl]) {
+
+        // Use the new open(_:options:completionHandler:) method
+
+        [[UIApplication sharedApplication] openURL:phoneUrl options:@{} completionHandler:^(BOOL success) {
+
+            if (!success) {
+
+                NSLog(@"Failed to make the call.");
+
+            }
+
+        }];
+
+    } else {
+
+        NSLog(@"Cannot make phone call. Please check the number or the device capabilities.");
+
+    }
+
+}
 
 @end
+
+ 
